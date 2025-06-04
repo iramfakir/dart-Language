dc555a996c8c81ffd24eeac51daee24c7574d2dd
Iram Fakir <iramfakir45@gmail.com>
2025-06-04 14:43:38 +0530

    commit

---
+++ variable3.dart
@@ -0,0 +1,10 @@
+void main()
+{
+  //without datatype
+  final name1="Iram Irfan Fakir";
+  print(name1);
+
+  //with datatype
+  final String name2="Rabiya Irfan Pendhari";
+  print(name2);
+}
---
+++ variable4.dart
@@ -0,0 +1,10 @@
+void main(){
+  //without datatype
+  const name1="Iram Irfan Fakir";
+  print(name1);
+
+  //with datatype
+  const name2="Rabiya Irfan Pendhari";
+  print(name2);
+
+}
