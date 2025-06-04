59706e93171b75e247fde429f3d925f04aef098d
Iram Fakir <iramfakir45@gmail.com>
2025-06-04 14:19:14 +0530

    commit

---
+++ variable1.dart
@@ -0,0 +1,11 @@
+void main(){
+  int num=10;
+  double num1=0.6;
+bool num2=true;
+String num3= "Iram" ,  num4="Rabiya";
+print(num);
+print(num1);
+print(num2);
+print(num3);
+print(num4);
+}
---
+++ variable2.dart
@@ -0,0 +1,8 @@
+import 'dart:ffi';
+
+void main(){
+  dynamic ex="Iram Fakir Fakir";
+  print(ex);
+  ex="Rabiya Irfan Fakir";
+  print(ex);
+}
