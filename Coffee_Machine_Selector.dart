a6a78184b075cfb6d612557f2656589b5d8e455a
Iram Fakir <iramfakir45@gmail.com>
2025-06-10 12:23:37 +0530

    commit

---
+++ Coffee_Machine_Selector.dart
@@ -0,0 +1,23 @@
+void main()
+{
+  String bavarege ='Coffee';
+  String time='Morning';
+  
+  if(bavarege=='Coffee' && time=='Morning')
+  {
+    print("Enjoy your Espresso");
+
+  }
+  else if(bavarege=='Coffee' && time=='Afternoon')
+  {
+    print("Enjoy your Latte ");
+  }
+  else if(bavarege=='Coffee' && time=='Evening')
+  {
+    print("Enjoy your Cappuccino");
+  }
+  else 
+  {
+    print("Enjoy your Tea");
+  }
+}
